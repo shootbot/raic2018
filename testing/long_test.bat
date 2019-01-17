270d3997ba6376048138cd4987e344bae9426db1
shootbot <timporin@gmail.com>
2019-01-18 00:26:03 +0600

    ver 0.5.1

--- testing/long_test.bat
+++ testing/long_test.bat
@@ -1,3 +1,3 @@
-start "" java -cp ".;..\target\java-cgdk-jar-with-dependencies.jar" Runner
+start "" java -cp ".;..\target\java-cgdk-jar-with-dependencies.jar" com.shootbot.raic2018.codeball.Runner
 start "" java -cp ".;0.4.0.jar" Runner 127.0.0.1 31000 0000000000000001
 codeball2018-windows\codeball2018.exe --p1-name current --p2-name 0.4.0 --p2 tcp-31000 --duration 100000
--- testing/run.bat
+++ testing/run.bat
@@ -1,2 +1,2 @@
-start "" java -cp ".;..\target\java-cgdk-jar-with-dependencies.jar" Runner
+start "" java -cp ".;..\target\java-cgdk-jar-with-dependencies.jar" com.shootbot.raic2018.codeball.Runner
 codeball2018-windows\codeball2018.exe --p1-name Time2Die
